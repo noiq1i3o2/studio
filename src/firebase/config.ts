2cf8ef8f0e1891ba9d21f0c15ad674b966ce5c48
El azizi Imrane <elaziziimrane60@gmail.com>
2025-12-17 19:58:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7770325357-f165e",
+  "appId": "1:529360301372:web:df127a09c39065f3b14239",
+  "apiKey": "AIzaSyA-PPbH2CMTNmRLAWfgz5xWJ-8Ooe01dh4",
+  "authDomain": "studio-7770325357-f165e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "529360301372"
+};
